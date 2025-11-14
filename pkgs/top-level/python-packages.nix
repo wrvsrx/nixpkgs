4d36df56cdaec7ef92c89f8559821cf68ac5207d
wrvsrx <wrvsrx@outlook.com>
2025-11-14 19:49:14 +0800

    python3Packages.langchain-text-splitters_0: init at 0.3.11

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -8085,6 +8085,8 @@ self: super: with self; {
 
   langchain-text-splitters = callPackage ../development/python-modules/langchain-text-splitters { };
 
+  langchain-text-splitters_0 = callPackage ../development/python-modules/langchain-text-splitters_0 { };
+
   langchain-xai = callPackage ../development/python-modules/langchain-xai { };
 
   langcodes = callPackage ../development/python-modules/langcodes { };
