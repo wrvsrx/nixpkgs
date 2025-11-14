660cdd771c61166b61b4b179606dce012c2df878
wrvsrx <wrvsrx@outlook.com>
2025-11-14 19:54:05 +0800

    python3Packages.langchain-tests_0: init at 0.3.21

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -8093,6 +8093,8 @@ self: super: with self; {
 
   langchain-tests = callPackage ../development/python-modules/langchain-tests { };
 
+  langchain-tests_0 = callPackage ../development/python-modules/langchain-tests_0 { };
+
   langchain-text-splitters = callPackage ../development/python-modules/langchain-text-splitters { };
 
   langchain-text-splitters_0 = callPackage ../development/python-modules/langchain-text-splitters_0 { };
